ec0ee805e11f1b45c5298ea4635101e0daae9ee4
Negin <ngnbageri@gmail.com>
2024-08-20 00:17:19 +0330

    toml files are working both rading and writing

--- Make Files for Questions/Make Questions File.py
+++ Make Files for Questions/Make Questions File.py
@@ -6,6 +6,5 @@ Created on Sun Aug 18 17:48:49 2024
 """
 
 import tomli
-# import tomli
-with open("questions.toml", mode="rb") as toml_file:
-    questions = tomli.load(toml_file)
+ with open("test.toml", mode="rb") as fp:
+     config = tomli.load(fp)
